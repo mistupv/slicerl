b3943eb3a3a3b4f2181824421e1dad684750b671
josilga <jsilva@dsic.upv.es>
2012-01-25 17:04:03 +0100

    Small change in a comment

--- slicErlang.erl
+++ slicErlang.erl
@@ -7,7 +7,7 @@
 % Millorar la precisio al obtenir les posibles funcions a aplicar utilitzant la inferencia de tipos (variables residuals)
 % Enganchar el input al nodo que defineix la funci贸 que es crida i no de al return
 % summaries
-% utilitzar la info del graph matching per confirmar que fan matching (stong i weak)						
+% utilitzar la info del graph matching per confirmar que fan matching (strong i weak)						
 %PROBLEMA: No s'estan afegint les variables que es declaren en arguments
 
 -module(slicErlang).
