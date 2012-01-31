101739fd2c2e85fb88125ca3d720d68c95055019
Cesar Tomas <csatomfr@hotmail.com>
2012-01-31 18:52:04 +0100

    Revert "solved problems with input arcs"
    This reverts commit 2788990b53aa62909b62d7d9686689cb9634845c.

--- temp_tt.erl
+++ temp_tt.erl
@@ -4,18 +4,4 @@
 
 main() -> case {} of {} -> main_0_CLAUSE() end.
 
-main_0_CLAUSE() ->
-    A = 3,
-    W = {A, {1, 2}},
-    transformed_call0(W),
-    {Y, {Z1, Z2}} = W.
-
-transformed_call0(VAR1) ->
-    case {VAR1} of
-      {VAR1} -> f(VAR1);
-      _ -> error
-    end.
-
-f(VAR1) -> case {VAR1} of {_} -> f_0_CLAUSE(VAR1) end.
-
-f_0_CLAUSE(_) -> 3.
+main_0_CLAUSE() -> W = {1, {1, 2}}.
